d950d3930762ccc0ab00c338daa35c4d2d5514b3
Bruce Ackerman <bcackerman@gmail.com>
2013-01-20 17:47:06 -0600

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Bruce Ackerman on 1/20/13.
+//  Copyright (c) 2013 Bruce Ackerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Bruce Ackerman on 1/20/13.
+//  Copyright (c) 2013 Bruce Ackerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
